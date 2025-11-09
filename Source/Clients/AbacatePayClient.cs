442e6d4ce5b40a9a00f2b566b6d14136900063d9
Richard Garcia <code.richardy@gmail.com>
2025-11-08 23:59:52 -0300

    introduces AbacatePayClient class for handling Pix charge session creation

---
+++ Source/Clients/AbacatePayClient.cs
@@ -0,0 +1,36 @@
+using System.Net.Http.Json;
+using System.Text.Json;
+
+using Vinder.Comanda.Internal.Contracts.Clients.Interfaces;
+using Vinder.Comanda.Internal.Contracts.Transport.External.AbacatePay;
+
+namespace Vinder.Comanda.Internal.Contracts.Clients;
+
+public sealed class AbacatePayClient(HttpClient httpClient) : IAbacatePayClient
+{
+    private readonly JsonSerializerOptions serializerOptions = new()
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        PropertyNameCaseInsensitive = true
+    };
+
+    public async Task<Result<PixChargeSessionScheme>> CreateChargeSessionAsync(
+        PixChargeScheme parameters, CancellationToken cancellation = default)
+    {
+        var response = await httpClient.PostAsJsonAsync("pixQrCode/create", parameters, cancellation);
+        var content = await response.Content.ReadAsStringAsync(cancellation);
+
+        if (response.IsSuccessStatusCode is false)
+        {
+            return Result<PixChargeSessionScheme>.Failure(AbacatePayErrors.OperationFailed);
+        }
+
+        var session = JsonSerializer.Deserialize<Response<PixChargeSessionScheme>>(content, serializerOptions);
+        if (session is null)
+        {
+            return Result<PixChargeSessionScheme>.Failure(AbacatePayErrors.InvalidContent);
+        }
+
+        return Result<PixChargeSessionScheme>.Success(session.Data);
+    }
+}
